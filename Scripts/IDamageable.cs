8dad111c53cb9872358d1bef7a29efe889c93d4f
Mikhail Shpirko <mikhail.shpirko@gmail.com>
2020-12-03 19:48:52 +0500

    code review

--- Scripts/IDamageable.cs
+++ Scripts/IDamageable.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//usually you use interfaces when you need to apply LSP (L in SOLID)
+//right now I don't think there is practical benefit of these interfaces
 public interface IDamageable
 {
     void TakeDamage(int damage);
