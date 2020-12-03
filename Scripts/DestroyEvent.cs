32a88c6550257143d2de2bb6e511de3b524256d5
Mikhail Shpirko <mikhail.shpirko@gmail.com>
2020-12-03 19:51:14 +0500

    code review

--- Scripts/DestroyEvent.cs
+++ Scripts/DestroyEvent.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//can't get the purpose of the script. should it destroy aother object not the current one?
 public class DestroyEvent : MonoBehaviour
 {
+    //why name is event?
     [SerializeField] private GameObject _destroyEvent;
-
+    
+    //I didn't find where this method used. why to make it public?
     public void Destroy()
     {
         Destroy(_destroyEvent.gameObject);
