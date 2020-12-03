291595a5d113d15abf21eb850159aca8de2035b1
Mikhail Shpirko <mikhail.shpirko@gmail.com>
2020-12-03 19:47:40 +0500

    code review

--- Scripts/EnemyHealth.cs
+++ Scripts/EnemyHealth.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Enemy;
 using UnityEngine;
 
+//script has mix of resposibilities - store HP and show animation
+//I would suggest to separate it and link them with events
 public class EnemyHealth : MonoBehaviour, IDamageable
 {
     [SerializeField] private EnemyAnimator _enemyAnimator;
@@ -11,8 +13,12 @@ public class EnemyHealth : MonoBehaviour, IDamageable
     public void TakeDamage(int damage)
     {
         _health -= damage;
+        //why can't this function call deadth function?
     }
-
+    
+    //why this is a public function?
+    //why name of the function is a noun?
+    //I have't found where in other scripts this function is called
     public void Death()
     {
         if (_health <= 0)
