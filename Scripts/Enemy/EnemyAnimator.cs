2adb669e85a593ab4a0a82f1d1ae817d5b963237
Mikhail Shpirko <mikhail.shpirko@gmail.com>
2020-12-03 20:08:06 +0500

    code review

--- Scripts/Enemy/EnemyAnimator.cs
+++ Scripts/Enemy/EnemyAnimator.cs
@@ -6,8 +6,11 @@ namespace Enemy
 {
     public class EnemyAnimator : MonoBehaviour
     {
+        //why public?
         public Animator _animator;
-
+        
+        //such methods is not a good idea as they are not readable
+        //name is illogical comparing to the actual process in function
         public void Move(bool canWalk)
         {
             _animator.SetBool("CanWalk", canWalk);
